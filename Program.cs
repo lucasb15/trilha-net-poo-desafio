893fc0f579a512379fc7150deb6e57166ac06bc0
Lucas Gomes Santos <lgsantos306@gmail.com>
2024-10-23 15:30:18 -0300

    Desafio comcluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("151320", "G10", "123", 8);
+nokia.MudarNumero("12345682");
+nokia.InstalarAplicativo("Facebook");
+nokia.Ligar();
+
+Iphone iphone = new Iphone("152145", "11", "1232", 8);
+iphone.InstalarAplicativo("Telegram");
+iphone.ReceberLigacao();
+
+Console.ReadKey(true);
